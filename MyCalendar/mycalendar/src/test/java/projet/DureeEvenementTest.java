4ebb1a34df2f17c28be30f8a61239ca3f8d4adb7
Baptiste <127935960+BaptisteLeRandom@users.noreply.github.com>
2026-03-25 11:42:28 +0100

    Ajout : test VO durée évenement

---
+++ MyCalendar/mycalendar/src/test/java/projet/DureeEvenementTest.java
@@ -0,0 +1,40 @@
+package projet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class DureeEvenementTest {
+
+    @Test
+    void construitUneDureeValide() {
+        DureeEvenement duree = new DureeEvenement(45);
+        assertEquals(45, duree.valeur());
+    }
+
+    @Test
+    void rejetteUneDureeNegative() {
+        assertThrows(IllegalArgumentException.class, () -> new DureeEvenement(-1));
+    }
+
+    @Test
+    void egaliteBaseeSurValeur() {
+        DureeEvenement d1 = new DureeEvenement(30);
+        DureeEvenement d2 = new DureeEvenement(30);
+        assertEquals(d1, d2);
+    }
+
+    @Test
+    void hashCodeCoherentAvecEquals() {
+        DureeEvenement d1 = new DureeEvenement(30);
+        DureeEvenement d2 = new DureeEvenement(30);
+        assertEquals(d1.hashCode(), d2.hashCode());
+    }
+
+    @Test
+    void toStringRetourneLaValeur() {
+        DureeEvenement duree = new DureeEvenement(30);
+        assertEquals("30", duree.toString());
+    }
+}
